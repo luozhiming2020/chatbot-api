d1dda07fd0dfbf45c4db3a2902e93a36611e6e18
luozhiming2020 <759792342@qq.com>
2024-08-16 10:23:59 +0800

    project init

---
+++ chatbot-api-infrastructure/src/main/java/com/spark/chatbot/ai/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.spark.chatbot.ai;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
--- chatbot-api-interfaces/src/test/java/com/spark/cahtbot/ai/test/ApiTest.java
+++
@@ -1,4 +0,0 @@
-package com.spark.cahtbot.ai.test;
-
-public class ApiTest {
-}
